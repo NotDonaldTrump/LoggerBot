20b37547541d11f0e37b8d260607c9ff061d208c
Parton <42082250+NotDonaldTrump@users.noreply.github.com>
2019-03-22 18:13:23 +1100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'replace_with_bot_token'; //Bot secret token here.
-
-exports.PREFIX = '>'; //Select your prefered command prefix, '>' is default.
-
-exports.SERVER = 'replace_with_server_name'; //Your server name.
